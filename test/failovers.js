7cbe9a092775fc1c006701134e324859db57e37f
Hugo Wetterberg <hugo@wetterberg.nu>
2015-01-14 11:57:47 +0100

    chore(tests): check that a leader change persists
    Adds a test that checks that the request following a redirect goes directly to the leader.

--- test/failovers.js
+++ test/failovers.js
@@ -25,10 +25,26 @@ lib.nock('http://127.0.0.1:4002')
     'date': 'Wed, 14 Jan 2015 09:37:39 GMT',
     'transfer-encoding': 'chunked' });
 
+lib.nock('http://127.0.0.1:4002')
+  .put('/v2/keys/bletcd-test/fooval', 'value=foovalue')
+  .reply(201, {'action':'set','node':{'key':'/bletcd-test/fooval','value':'foovalue','modifiedIndex':279,'createdIndex':279}}, {
+    'content-type': 'application/json',
+    'x-etcd-index': '279',
+    'x-raft-index': '26392',
+    'x-raft-term': '1',
+    'date': 'Wed, 14 Jan 2015 09:37:39 GMT',
+    'transfer-encoding': 'chunked' });
+
 suite('Failovers', function testClient() {
   var etcd = lib.Etcd();
 
   test('Client follows redirects to leader', function createNode(done) {
+    etcd.set('bletcd-test/fooval', 'foovalue', function createResult(error) {
+      done(error);
+    });
+  });
+
+  test('Next request goes directly to leader', function createNode(done) {
     etcd.set('bletcd-test/fooval', 'foovalue', function createResult(error) {
       done(error);
     });
